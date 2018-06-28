45877ac3004a7c18267175b3431932ec2939ad5c
wangdiyuan <wangdiyuan@smartisan.com>
2018-06-28 15:20:19 +0800

    add show result

--- app/src/main/java/com/tfkj/opencv3/MainActivity.java
+++ app/src/main/java/com/tfkj/opencv3/MainActivity.java
@@ -3,15 +3,18 @@ package com.tfkj.opencv3;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,10 +26,15 @@ public class MainActivity extends AppCompatActivity {
 
     private ProgressDialog dlg;
 
+    ImageView mImageView;
+    ArrayList<String> mResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.imageView);
 
         dlg = new ProgressDialog(this);
 
@@ -44,8 +52,40 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(chooserIntent, REQUEST_OPEN_IMAGE);
             }
         });
+
+        findViewById(R.id.show_result).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mResult == null || mResult.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"结果空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                intent.putStringArrayListExtra("result", mResult);
+                startActivity(intent);
+            }
+        });
     }
 
+
+    private void setPic(String pic) {
+        int targetW = mImageView.getWidth();
+        int targetH = mImageView.getHeight();
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(pic, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(photoW / targetW, photoH / targetH);
+
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        mImageView.setImageBitmap(BitmapFactory.decodeFile(pic, bmOptions));
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -62,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
                     int colIndex = cursor.getColumnIndex(filePathColumn[0]);
                     final String imagePath = cursor.getString(colIndex);
                     cursor.close();
+                    setPic(imagePath);
+
 
                     dlg.setMessage("正在查找");
                     dlg.setCancelable(false);
@@ -72,14 +114,20 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             Log.d("FIND_OBJ", "start find");
-                            final ArrayList<String> count = find_objects(imagePath);
-                            Log.d("FIND_OBJ", "count: " + count);
+                            mResult = find_objects(imagePath);
+                            Log.d("FIND_OBJ", "count: " + mResult.size());
                             getWindow().getDecorView().post(new Runnable() {
                                 @Override
                                 public void run() {
                                     dlg.dismiss();
 
-                                    Toast.makeText(getApplicationContext(),"找到：" + count + "个", Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getApplicationContext(),"找到：" + mResult.size() + "个", Toast.LENGTH_SHORT).show();
+
+                                    Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                                    intent.putStringArrayListExtra("result", mResult);
+                                    startActivity(intent);
+
+
                                 }
                             });
                         }
