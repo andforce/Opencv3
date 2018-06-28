a8e182fa6cf0c816b26a59cb0073fdd7e91e6afc
wangdiyuan <wangdiyuan@smartisan.com>
2018-06-28 13:11:30 +0800

    update

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,70 +7,79 @@
 using namespace cv;
 using namespace std;
 
-#define TAG "lm"
+#define TAG "FIND_OBJ"
 #define LOGD(...) __android_log_print(ANDROID_LOG_ERROR,TAG ,__VA_ARGS__)
 
-extern "C" {
-    JNIEXPORT void JNICALL Java_com_tfkj_opencv3_MainActivity_gray(JNIEnv *env, jclass type) {
-        Mat image = imread("/sdcard/101/rock.png");
-        Mat dst, dst1;
-        cvtColor(image, dst, COLOR_BGR2BGRA);
+bool isNormal(const vector<vector<Point>> &contours, int i);
 
-        vector<Mat> channels;
-        split(dst, channels);
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_tfkj_opencv3_MainActivity_find_1objects(JNIEnv *env, jclass type, jstring imagePath_) {
+    const char *imagePath = env->GetStringUTFChars(imagePath_, 0);
 
-        for (int i = 0; i < channels[3].rows; i++){
-            for (int j = 0; j < channels[3].cols; j++){
-                channels[3].at<uchar>(i, j) = 0;
-            }
+    LOGD("start find-----------------%s", imagePath);
+
+    Mat image = imread(imagePath);
+    Mat dst, dst1;
+    cvtColor(image, dst, COLOR_BGR2BGRA);
+
+
+    vector<Mat> channels;
+    split(dst, channels);
+
+    for (int i = 0; i < channels[3].rows; i++){
+        for (int j = 0; j < channels[3].cols; j++){
+            channels[3].at<uchar>(i, j) = 0;
         }
-
-        merge(channels, dst1);
-
-        //imwrite("/Users/apple/Desktop/dst1.png", dst1);
-
-        Mat gray;
-        cvtColor(image, gray, COLOR_BGR2GRAY);
-
-        Mat thresh;
-        threshold(gray, thresh, 0, 255, THRESH_BINARY_INV|THRESH_OTSU);
-
-        morphologyEx(thresh, thresh, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(15, 15)), Point(-1,-1), 2);
-        //morphologyEx(thresh, thresh, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)), Point(-1,-1), 2);
-
-        vector<vector<Point>> contours;
-        findContours(thresh, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-        printf("contours.size() = %lu\n", contours.size());
-
-        for (int i = 0; i< contours.size(); i++) {
-            printf("contourArea = %f\n", contourArea(contours[i]));
-            if (contourArea(contours[i]) < 10000) {
-                continue;
-            }
-
-            for (int k = 0; k < dst1.rows; k++){
-                for (int j = 0; j < dst1.cols; j++){
-                    if (pointPolygonTest(contours[i], Point2f(j, k), false) > 0) {
-                        dst1.at<Vec4b>(k, j)[3] = 255;
-                    }
-
-                }
-            }
-        }
-
-        for (int i = 0; i< contours.size(); i++) {
-            if (contourArea(contours[i]) < 10000) {
-                continue;
-            }
-
-            Rect rect = boundingRect(contours[i]);
-            //rectangle(image, rect, Scalar(0, 0, 255), 1);
-
-            ostringstream stream;
-            stream << i;
-            imwrite("/sdcard/101/" + stream.str() + ".png", dst1(rect));
-        }
-
-        imwrite("/sdcard/101/dst1.png", dst1);
     }
+
+    merge(channels, dst1);
+
+    //imwrite("/Users/apple/Desktop/dst1.png", dst1);
+
+    Mat gray;
+    cvtColor(image, gray, COLOR_BGR2GRAY);
+
+    Mat thresh;
+    threshold(gray, thresh, 0, 255, THRESH_BINARY_INV|THRESH_OTSU);
+
+    imwrite("/storage/emulated/0/Download/bin.png", thresh);
+
+    morphologyEx(thresh, thresh, MORPH_CLOSE, getStructuringElement(MORPH_RECT, Size(15, 15)), Point(-1,-1), 2);
+    //morphologyEx(thresh, thresh, MORPH_OPEN, getStructuringElement(MORPH_RECT, Size(3, 3)), Point(-1,-1), 2);
+
+    vector<vector<Point>> contours;
+    findContours(thresh, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+    LOGD("contours.size() = %u\n", contours.size());
+
+    int count = 0;
+    for (int i = 0; i< contours.size(); i++) {
+        LOGD("contourArea = %f\n", contourArea(contours[i]));
+        if (isNormal(contours, i)) {
+            continue;
+        }
+
+        for (int k = 0; k < dst1.rows; k++){
+            for (int j = 0; j < dst1.cols; j++){
+                if (pointPolygonTest(contours[i], Point2f(j, k), false) > 0) {
+                    dst1.at<Vec4b>(k, j)[3] = 255;
+                }
+
+            }
+        }
+
+        Rect rect = boundingRect(contours[i]);
+        //rectangle(image, rect, Scalar(0, 0, 255), 1);
+
+        ostringstream stream;
+        stream << i;
+        count ++;
+        imwrite("/storage/emulated/0/Download/" + stream.str() + ".png", dst1(rect));
+    }
+
+    env->ReleaseStringUTFChars(imagePath_, imagePath);
+
+    return count;
 }
+
+bool isNormal(const vector<vector<Point>> &contours, int i) { return contourArea(contours[i]) < 1000; }
--- app/src/main/java/com/tfkj/opencv3/MainActivity.java
+++ app/src/main/java/com/tfkj/opencv3/MainActivity.java
@@ -1,20 +1,93 @@
 package com.tfkj.opencv3;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_OPEN_IMAGE = 1;
 
     static {
         System.loadLibrary("native-lib");
     }
 
+    private ProgressDialog dlg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        gray();
+
+        dlg = new ProgressDialog(this);
+
+        findViewById(R.id.choose_image).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getPictureIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getPictureIntent.setType("image/*");
+                Intent pickPictureIntent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                Intent chooserIntent = Intent.createChooser(getPictureIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {
+                        pickPictureIntent
+                });
+                startActivityForResult(chooserIntent, REQUEST_OPEN_IMAGE);
+            }
+        });
     }
 
-    public static native void gray();
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_OPEN_IMAGE:
+                if (resultCode == RESULT_OK) {
+                    Uri imgUri = data.getData();
+                    String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+                    Cursor cursor = getContentResolver().query(imgUri, filePathColumn,
+                            null, null, null);
+                    cursor.moveToFirst();
+
+                    int colIndex = cursor.getColumnIndex(filePathColumn[0]);
+                    final String imagePath = cursor.getString(colIndex);
+                    cursor.close();
+
+                    dlg.setMessage("正在查找");
+                    dlg.setCancelable(false);
+                    dlg.setIndeterminate(true);
+                    dlg.show();
+
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.d("FIND_OBJ", "start find");
+                            final int count = find_objects(imagePath);
+                            Log.d("FIND_OBJ", "count: " + count);
+                            getWindow().getDecorView().post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    dlg.dismiss();
+
+                                    Toast.makeText(getApplicationContext(),"找到：" + count + "个", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }).start();
+                }
+                break;
+        }
+    }
+
+    //public static native void gray();
+
+    public static native int find_objects(String imagePath);
 }
