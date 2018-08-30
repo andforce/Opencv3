1634390730003bf521fdfd617410118a9b327ff1
wangdiyuan <wangdiyuan@smartisan.com>
2018-08-30 15:54:25 +0800

    update

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <android/log.h>
 #include <opencv2/opencv.hpp>
+#include "cpp_java_utils.h"
 
 using namespace cv;
 using namespace std;
@@ -11,30 +12,7 @@ using namespace std;
 
 bool isNormal(const vector<vector<Point>> &contours, int i);
 
-jobject cpp2java(JNIEnv *env, std::vector<std::string> vector) {
 
-    static jclass java_util_ArrayList;
-    static jmethodID java_util_ArrayList_;
-    jmethodID java_util_ArrayList_size;
-    jmethodID java_util_ArrayList_get;
-    jmethodID java_util_ArrayList_add;
-
-    java_util_ArrayList = static_cast<jclass>(env->NewGlobalRef(
-            env->FindClass("java/util/ArrayList")));
-    java_util_ArrayList_ = env->GetMethodID(java_util_ArrayList, "<init>", "(I)V");
-    java_util_ArrayList_size = env->GetMethodID(java_util_ArrayList, "size", "()I");
-    java_util_ArrayList_get = env->GetMethodID(java_util_ArrayList, "get", "(I)Ljava/lang/Object;");
-    java_util_ArrayList_add = env->GetMethodID(java_util_ArrayList, "add", "(Ljava/lang/Object;)Z");
-
-
-    jobject result = env->NewObject(java_util_ArrayList, java_util_ArrayList_, vector.size());
-    for (std::string s: vector) {
-        jstring element = env->NewStringUTF(s.c_str());
-        env->CallBooleanMethod(result, java_util_ArrayList_add, element);
-        env->DeleteLocalRef(element);
-    }
-    return result;
-}
 
 extern "C"
 JNIEXPORT jobject JNICALL
@@ -108,7 +86,7 @@ Java_com_tfkj_opencv3_MainActivity_find_1objects(JNIEnv *env, jclass type, jstri
 
     env->ReleaseStringUTFChars(imagePath_, imagePath);
 
-    return cpp2java(env, result);
+    return vector2java_util_ArrayList(env, result);
 }
 
 bool isNormal(const vector<vector<Point>> &contours, int i) {
